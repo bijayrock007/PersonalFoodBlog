279b0f7c0c5e0f2dc58b98e60e2596ecf32bf136
bijayrock007 <149135039+bijayrock007@users.noreply.github.com>
2023-12-01 16:47:18 +0800

    Update fooblog.js

--- fooblog.js
+++ fooblog.js
@@ -9,3 +9,14 @@ function closeChat(){
     minimizeChat.style.display='flex';
     chat.style.display='none'
 }
+function menuopen(){
+    let HamMenu=document.getElementsByClassName('HamMenu')
+    if(HamMenu[0].style.display=='flex'){
+        HamMenu[0].style.display='none';
+    }
+    else{
+        HamMenu[0].style.display='flex';
+    }
+    
+    console.log("xxxx");
+}
